d1d21c73fe93eb8032d7907953dccdea21898b78
Casey Link <unnamedrambler@gmail.com>
2022-10-14 17:42:04 +0200

    Try to use a hickory based converter

--- cleanup.cljs
+++ cleanup.cljs
@@ -2,7 +2,10 @@
   (:require ["svgo" :as svgo]
             ["fs" :as fs]
             ["path" :as path]
-            [taipei-404.html :refer [html->hiccup]]
+            [converter :refer [html->hiccup]]
+
+
+            ;; [taipei-404.html :refer [html->hiccup]]
             [nbb.core :refer [*file*]]))
 
 (def settings {:multipass true
@@ -22,5 +25,5 @@
     (let [svg-string (str (fs/readFileSync svg-path))
           settings-js (-> settings (merge {:path svg-path}) clj->js)
           {:keys [data path info]} (-> svg-string (svgo/optimize settings-js) (js->clj   {:keywordize-keys true}))]
-      ;; (println data)
-      (println (-> r :data html->hiccup)))))
+      (println (-> r :data html->hiccup))
+      )))
